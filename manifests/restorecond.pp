cb0193b1c1c9ad87eda603799ad4ca53708a4849
Stephen Price <sprice@monsooncommerce.com>
2014-08-13 14:11:09 -0700

    lint: params class

--- manifests/restorecond.pp
+++ manifests/restorecond.pp
@@ -2,11 +2,49 @@
 # Class selinux::restorecond
 #
 class selinux::restorecond (
-  $config_file       = $selinux::params::restorecond_config_file,
-  $config_file_mode  = $selinux::params::restorecond_config_file_mode,
-  $config_file_owner = $selinux::params::restorecond_config_file_owner,
-  $config_file_group = $selinux::params::restorecond_config_file_group,
-) inherits selinux::params {
+  $config_file                   = $selinux::params::restorecond_config_file,
+  $config_file_mode              = $selinux::params::restorecond_config_file_mode,
+  $config_file_owner             = $selinux::params::restorecond_config_file_owner,
+  $config_file_group             = $selinux::params::restorecond_config_file_group,
+  $sx_mod_dir                    = 'UNSET',
+  $mode                          = 'UNSET',
+  $restorecond_config_file       = 'UNSET',
+  $restorecond_config_file_mode  = 'UNSET',
+  $restorecond_config_file_owner = 'UNSET',
+  $restorecond_config_file_group = 'UNSET',
+) {
+  include selinux::params
+
+  $sx_mod_dir_real = $sx_mod_dir ? {
+    'UNSET' => $::selinux::params::sx_mod_dir,
+    default => $sx_mod_dir,
+  }
+
+  $mode_real = $mode ? {
+    'UNSET' => $::selinux::params::mode,
+    default => $mode,
+  }
+
+  $restorecond_config_file_real = $restorecond_config_file ? {
+    'UNSET' => $::selinux::params::$restorecond_config_file,
+    default => $restorecond_config_file,
+  }
+
+  $restorecond_config_file_mode_real = $restorecond_config_file_mode ? {
+    'UNSET' => $::selinux::params::$restorecond_config_file_mode,
+    default => $restorecond_config_file_mode,
+  }
+
+  $restorecond_config_file_owner_real = $restorecond_config_file_owner ? {
+    'UNSET' => $::selinux::params::$restorecond_config_file_owner,
+    default => $restorecond_config_file_owner,
+  }
+
+  $restorecond_config_file_group_real = $restorecond_config_file_group ? {
+    'UNSET' => $::selinux::params::$restorecond_config_file_group,
+    default => $restorecond_config_file_group,
+  }
+
   class{'selinux::restorecond::install':} ->
   class{'selinux::restorecond::config':} ~>
   class{'selinux::restorecond::service':}
--- manifests/restorecond/config.pp
+++ manifests/restorecond/config.pp
@@ -5,20 +5,20 @@ class selinux::restorecond::config {
 
   concat { $selinux::restorecond::config_file:
     ensure => present,
-    mode   => $selinux::restorecond::config_file_mode,
-    owner  => $selinux::restorecond::config_file_owner,
-    group  => $selinux::restorecond::config_file_group,
+    mode   => $selinux::restorecond::restorecond_config_file_mode_real,
+    owner  => $selinux::restorecond::config_file_owner_real,
+    group  => $selinux::restorecond::config_file_group_real,
     notify => Service['restorecond'],
   }
 
   concat::fragment {'restorecond_config_header':
-    target  => $selinux::restorecond::config_file,
+    target  => $selinux::restorecond::config_file_real,
     content => "# File Managed by Puppet\n",
     order   => '01'
   }
 
   concat::fragment {'restorecond_config_default':
-    target  => $selinux::restorecond::config_file,
+    target  => $selinux::restorecond::config_file_real,
     source  => 'puppet:///modules/selinux/restorecond.conf',
     order   => '05'
   }
