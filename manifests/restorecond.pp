935250e2a7295e89f58813cf3fef300e276d42d5
Stephen Price <sprice@monsooncommerce.com>
2014-08-13 13:57:19 -0700

    lint stuff
    lint: params class
    
    oops
    
    Revert "linting changes around inheriting params class"
    
    This reverts commit 93064e8944ac2ac4f2650f93e7cd8e6b4bf5733a.
    
    Revert "lint: params class"
    
    This reverts commit cb0193b1c1c9ad87eda603799ad4ca53708a4849.
    
    remove unneeded inheritance
    
    linting params class

--- manifests/restorecond.pp
+++ manifests/restorecond.pp
@@ -6,7 +6,7 @@ class selinux::restorecond (
   $config_file_mode  = $selinux::params::restorecond_config_file_mode,
   $config_file_owner = $selinux::params::restorecond_config_file_owner,
   $config_file_group = $selinux::params::restorecond_config_file_group,
-) inherits selinux::params {
+) {
   class{'selinux::restorecond::install':} ->
   class{'selinux::restorecond::config':} ~>
   class{'selinux::restorecond::service':}
