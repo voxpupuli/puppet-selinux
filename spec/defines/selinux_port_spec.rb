63a517006de5e2c301f0e357ce9be56df210831d
David LeVene <david.levene@netspot.com.au>
2015-04-23 16:36:21 +0930

    If you have the same port allowed on both udp & tcp puppet manifest will fail as you have a duplicate name, this change adds the protocol name into port definition

--- spec/defines/selinux_port_spec.rb
+++ spec/defines/selinux_port_spec.rb
@@ -7,7 +7,7 @@ describe 'selinux::port' do
   ['tcp', 'udp', 'tcp6', 'udp6'].each do |protocol|
     context "valid protocol #{protocol}" do
       let(:params) { { :context => 'http_port_t', :port => 8080, :protocol => protocol } }
-      it { should contain_exec('add_http_port_t_8080').with(:command => "semanage port -a -t http_port_t -p #{protocol} 8080") }
+      it { should contain_exec("add_http_port_t_8080_#{protocol}").with(:command => "semanage port -a -t http_port_t -p #{protocol} 8080") }
     end
   end
 
@@ -17,8 +17,8 @@ describe 'selinux::port' do
   end
 
   context 'no protocol' do
-    let(:params) { { :context => 'http_port_t', :port => 8080 } }
-    it { should contain_exec('add_http_port_t_8080').with(:command => 'semanage port -a -t http_port_t 8080') }
+    let(:params) { { :context => 'http_port_t', :port => 8080} }
+    it { should contain_exec("add_http_port_t_8080").with(:command => 'semanage port -a -t http_port_t 8080') }
   end
 
 end
