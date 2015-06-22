cd14d02f10f69da56343a93068338ba3aa50f3c4
Belmin Fernandez <belmin.fernandez@cuny.edu>
2015-06-22 12:25:38 -0400

    awk escaping + confine fix

--- lib/facter/selinux_custom_policy.rb
+++ lib/facter/selinux_custom_policy.rb
@@ -5,8 +5,8 @@
 require 'facter'
 
 Facter.add(:selinux_custom_policy) do
-  confine :kernel => 'Linux', :osfamily => 'RedHat', :operatingsystemmajrelease => '7', :selinux => true
+  confine :kernel => 'Linux', :osfamily => 'RedHat', :operatingsystemmajrelease => '7', :selinux => ['true', true]
   setcode do
-    Facter::Util::Resolution.exec("sestatus | grep 'Loaded policy name' | awk '{ print $4 }'")
+    Facter::Util::Resolution.exec("sestatus | grep 'Loaded policy name' | awk '{ print \$4 }'")
   end
 end
