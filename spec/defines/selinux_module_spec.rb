5794ddeeb062d39a9c40acb58032fbf3f753431a
Philip Jay <pjay@odecee.com.au>
2015-09-30 13:41:29 +1000

    Fix for selinux::module absent case failed notify
    Before this, selinux::module ensure => 'absent' would fail because:
    - Exec["${name}-buildmod"] wouldn't be created
    - Exec["${name}-checkloaded"] therefore couldn't notify it
    
    Also:
    - Add rspec testing for selinux::module
    - Add custom fact selinux_config_policy to spec_helper

---
+++ spec/defines/selinux_module_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'selinux::module' do
+  let(:title) { 'mymodule' }
+  include_context 'RedHat 7'
+
+  context 'present case' do
+
+    let(:params) {{
+      :source => 'test_value'
+    }}
+
+    it { should contain_exec("mymodule-checkloaded").
+           that_notifies("Exec[mymodule-buildmod]")
+    }
+
+  end  # context
+  
+  context 'absent case' do
+
+    let(:params) {{
+      :source => 'test_value',
+      :ensure => 'absent'
+    }}
+
+    it { should_not contain_exec("mymodule-checkloaded").
+           that_notifies("Exec[mymodule-buildmod]")
+    }
+
+  end  # context
+
+end  # describe
