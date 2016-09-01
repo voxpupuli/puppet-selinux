3feb4c63a9902f71be9f4f8242078d6c83f950bb
Markus Linnala <Markus.Linnala@cybercom.com>
2016-09-01 14:23:47 +0300

    additional tests to get coverage

--- spec/classes/selinux_restorecond_spec.rb
+++ spec/classes/selinux_restorecond_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'selinux::restorecond' do
   include_context 'RedHat 7'
 
+  it { should contain_class('selinux::restorecond') }
   it { should contain_class('selinux::restorecond::config') }
   it { should contain_class('selinux::restorecond::service') }
 end
