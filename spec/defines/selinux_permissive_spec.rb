242a0799f680daa034343ada604d8353c62c8647
Rudy YAYON <ryayon@outlook.com>
2016-11-07 18:19:21 +0100

    Fixes Issue-130 - No rspec for permissive (#131)
    * Fixes Issue-130 - No rspec for permissive
    
    * Fix `RuboCop failed`
    
    * Fix Rubocop issue
    
    `spec/defines/selinux_permissive_spec.rb:18:1: C: Style/EmptyLinesAroundBlockBody: Extra empty line detected at block body end.`

---
+++ spec/defines/selinux_permissive_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'selinux::permissive' do
+  let(:title) { 'mycontextp' }
+  include_context 'RedHat 7'
+
+  context 'context allow-oddjob_mkhomedir_t to permissive' do
+    let(:params) do
+      {
+        context: 'oddjob_mkhomedir_t'
+      }
+    end
+
+    it do
+      is_expected.to contain_exec('add_oddjob_mkhomedir_t').with(command: 'semanage permissive -a oddjob_mkhomedir_t')
+    end
+  end
+end
