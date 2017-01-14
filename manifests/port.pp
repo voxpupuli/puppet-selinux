051589a90c556ae712334db78e39e1b1674a9553
Jarkko Oranen <oranenj@iki.fi>
2017-01-14 21:16:48 +0200

    Use new selinux_port type

--- manifests/port.pp
+++ manifests/port.pp
@@ -1,4 +1,4 @@
-# selinux::fcontext
+# selinux::port
 #
 # This method will manage a local network port context setting, and will
 # persist it across reboots.
@@ -12,15 +12,16 @@
 #   }
 #
 # @param context A port-context name
-# @param protocol Either tcp or udp. If unset, omits -p flag from semanage.
-# @param port An network port number, like '8514'
-# @param argument An argument for semanage port. Default: "-a"
+# @param protocol Either tcp or udp.
+# @param port An network port number, like '8514', or a range like '1234-4321'
+# @param argument *deprecated*, ignored
 #
 define selinux::port (
   $context,
   $port,
+  $ensure = 'present',
   $protocol = undef,
-  $argument = '-a',
+  $argument = undef,
 ) {
 
   include ::selinux
@@ -29,24 +30,11 @@ define selinux::port (
   Selinux::Port[$title] ->
   Anchor['selinux::end']
 
-  validate_re("${port}", '^[0-9]+(-[0-9]+)?$') # lint:ignore:only_variable_string
-
-  if $protocol {
-    validate_re($protocol, ['^tcp$', '^udp$'])
-    $protocol_switch = ['-p', $protocol]
-    $protocol_check = "${protocol} "
-    $port_exec_command = "add_${context}_${port}_${protocol}"
-  } else {
-    $protocol_switch = []
-    $protocol_check = '' # lint:ignore:empty_string_assignment variable is used to create regexp and undef is not possible
-    $port_exec_command = "add_${context}_${port}"
-  }
-
-  exec { $port_exec_command:
-    command => shellquote('semanage', 'port', $argument, '-t', $context, $protocol_switch, "${port}"), # lint:ignore:only_variable_string port can be number and we need to force it to be string for shellquote
-    # This works because there seems to be more than one space after protocol and before first port
-    unless  => sprintf('semanage port -l | grep -E %s', shellquote("^${context}  *${protocol_check}.* ${port}(\$|,)")),
-    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-    require => Class['selinux::package'],
+  validate_re($protocol, ['^tcp6?$', '^udp6?$'])
+  selinux_port {"${protocol}_${port}":
+    ensure   => $ensure,
+    ports    => $port,
+    context  => $context,
+    protocol => $protocol,
   }
 }
