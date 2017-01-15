5eefda8d8f868c256ca0a5bbbccee0352f52eaf1
Jarkko Oranen <oranenj@iki.fi>
2017-01-15 09:13:26 +0200

    Use types in port.pp

--- manifests/port.pp
+++ manifests/port.pp
@@ -6,34 +6,40 @@
 #
 # @example Add port-context syslogd_port_t to port 8514/tcp
 #   selinux::port { 'allow-syslog-relp':
+#     ensure   => 'present',
 #     context  => 'syslogd_port_t',
 #     protocol => 'tcp',
 #     port     => '8514',
 #   }
 #
 # @param context A port-context name
-# @param protocol Either tcp or udp.
+# @param protocol Either 'tcp', 'udp', 'ipv4' or 'ipv6'
 # @param port An network port number, like '8514', or a range like '1234-4321'
-# @param argument *deprecated*, ignored
 #
 define selinux::port (
-  $context,
-  $port,
-  $ensure = 'present',
-  $protocol = undef,
-  $argument = undef,
+  String                             $context,
+  Variant[Integer[1,65535],String]   $port,
+  Enum['tcp', 'udp', 'ipv4', 'ipv6'] $protocol,
+  Enum['present', 'absent']          $ensure = 'present',
 ) {
 
   include ::selinux
 
-  Anchor['selinux::module post'] ->
-  Selinux::Port[$title] ->
-  Anchor['selinux::end']
+  if $ensure == 'present' {
+    Anchor['selinux::module post'] ->
+    Selinux::Port[$title] ->
+    Anchor['selinux::end']
+  } elsif $ensure == 'absent' {
+    Class['selinux::config'] ->
+    Selinux::Port[$title] ->
+    Anchor['selinux::module pre']
+  } else {
+    fail('Unexpected $ensure value')
+  }
 
-  validate_re($protocol, ['^tcp6?$', '^udp6?$'])
   selinux_port {"${protocol}_${port}":
     ensure   => $ensure,
-    ports    => $port,
+    ports    => $port, # type definition will validate this better in case it's a range
     context  => $context,
     protocol => $protocol,
   }
