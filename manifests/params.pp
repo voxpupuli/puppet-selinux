ebb772aee1b38419a3d3f1fc5eff5070024f5abd
Michael Watters <michael.watters@dart.biz>
2019-06-26 11:19:34 -0400

    Change - Update module parameters to support older Fedora releases
    Recent changes to the manifest/params.pp file have caused breakage
    on servers running older Fedora release.  Update manifest to avoid
    this in environments that still need support for legacy hosts.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,21 +5,31 @@
 class selinux::params {
   $module_build_root = "${facts['puppet_vardir']}/puppet-selinux"
 
-  case $facts['os']['family'] {
-    'RedHat': {
-      if $facts['os']['name'] == 'Amazon' {
-        $package_name = 'policycoreutils'
-      } else {
-        $package_name = $facts['os']['release']['major'] ? {
-          '5'     => 'policycoreutils',
-          '6'     => 'policycoreutils-python',
-          '7'     => 'policycoreutils-python',
-          default => 'policycoreutils-python-utils',
-        }
+  case $facts['os']['name'] {
+    'Amazon': {
+      $package_name = 'policycoreutils'
+    }
+
+    'CentOS', 'RedHat': {
+      $package_name = $facts['os']['release']['major'] ? {
+        '5'     => 'policycoreutils',
+        '6'     => 'policycoreutils-python',
+        '7'     => 'policycoreutils-python',
+        default => 'policycoreutils-python-utils',
       }
     }
+
+    'Fedora': {
+      $sx_fs_mount = '/sys/fs/selinux'
+      $package_name = $facts['os']['release']['major'] ? {
+        /19|20/  => 'policycoreutils-python',
+        /2[1-3]/ => 'policycoreutils-devel',
+        default  => 'policycoreutils-python-utils',
+      }
+    }
+
     default: {
-      fail("${facts['os']['family']} is not supported")
+      fail("${facts['os']['name']} is not supported by ${name}")
     }
   }
 }
