0af46eaea17723787663e737a95f492e497fe8f6
Emily Rowlands <emily@erowl.net>
2026-03-04 10:29:33 +0000

    selinux::login: Add basic spec

---
+++ spec/defines/selinux_login_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'selinux::login' do
+  let(:title) { 'myapp' }
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) do
+        override_facts(os_facts, 'os' => { 'selinux' => { 'enabled' => true } })
+      end
+
+      context 'ordering' do
+        let(:params) do
+          {
+            selinux_login_name: 'localuser',
+            selinux_user: 'staff_u',
+          }
+        end
+
+        it { is_expected.to contain_selinux__login('myapp').that_requires('Anchor[selinux::module post]') }
+        it { is_expected.to contain_selinux__login('myapp').that_comes_before('Anchor[selinux::end]') }
+      end
+
+      context 'valid user' do
+        let(:params) do
+          {
+            selinux_login_name: 'localuser',
+            selinux_user: 'staff_u',
+          }
+        end
+
+        it { is_expected.to contain_selinux_login('localuser').with(selinux_login_name: 'localuser', selinux_user: 'staff_u') }
+      end
+    end
+  end
+end
